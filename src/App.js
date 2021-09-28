5295271258056b4d2472faa314e5af906f2d2469
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-28 11:12:13 -0400

    implemented navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/NavBar/Navbar";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+           <About />
+          </Route>
+          <Route exact path="/contact">
+          <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
