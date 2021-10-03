d3f847dd48da7059a7f6ef1781f261fea254dd7b
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-10-02 23:20:27 -0400

    implemented POST method to create new review

--- src/components/Pages/ReviewForm.js
+++ src/components/Pages/ReviewForm.js
@@ -12,8 +12,34 @@ function ReviewForm() {
            movie_id: ""
        })
 
-      
-  
+       function onHandleChange(e){
+           setReviewForm({ ...reviewForm, [e.target.name]: e.target.value })
+       }
+
+        function onSubmitReviews(e) {
+    e.preventDefault();
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+       comment: reviewFrom.comment,
+       rating: reviewFrom.rating,
+       username: reviewFrom.username,
+       movie_id: reviewFrom.movie_id
+
+      }),
+    };
+
+    fetch(`${baseURL}/reviews`, config)
+      .then((res) => res.json())
+      .then((newReview) => {
+        const newReviews = [reviewForm, newReview];
+        setReviewForm(newReviews);
+        history.push("/courses");
+      });
+  }
 
 
      
