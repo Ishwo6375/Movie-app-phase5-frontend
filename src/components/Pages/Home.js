66af1fbe968d6fb9e80ee5e51e46555ad1ca3053
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-10-04 10:56:14 -0400

    addedd style to home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -8,11 +8,21 @@ function Home() {
             <div className="home-content">
                  <h1 className="home-header"><span className="span-7">Watch Latest</span>
                      <br />
+                     <br />
+                     <h1>
+
                      <span className="span-6">Movie Trailers</span>
+                     </h1>
                      <br />
 
-                    Tell your friends <span className="span-7"></span>what's good
+                     <h2><span className="span-8">Review</span>  
+                     <br />
+                     <span className="span-6"> Movie</span></h2>
+
+                    <span className="span-8">Tell your friends </span><span className="span-7">what's good</span>
                  </h1>
+
+                 <br />
 
                  <div>
                    <Link className="btn btn-primary" to={"/Movies"}>
