57eb2f034887ddc2a31fa887b628f4d266d39f47
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-10-04 10:28:08 -0400

    added content to home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "../styles/home.css";
 
 function Home() {
     return (
-        <div>
-            <div></div>
+        <div className="home-main">
+            <div className="home-content">
+                 <h1 className="home-header"><span className="span-7">Watch Latest</span>
+                     <br />
+                     <span className="span-6">Movie Trailers</span>
+                     <br />
+
+                    Tell your friends <span className="span-7"></span>what's good
+                 </h1>
+
+                 <div>
+                   <Link className="btn btn-primary" to={"/Movies"}>
+                <h4>Get Started it's free</h4>
+              </Link>
+                 </div>
+            </div>
         </div>
     )
 }
