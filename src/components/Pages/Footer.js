8e8bb5cf34dc74389fa723c82903089ae983c21f
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-10-05 14:28:12 -0400

    added _redirects file to public folder

--- src/components/Pages/Footer.js
+++ src/components/Pages/Footer.js
@@ -41,7 +41,7 @@ function Footer() {
                           <li><a href="https://github.com/Ishwo6375"><span className="span-11"><FaGithub /></span>Github</a></li>
                           <li><a href="mailto: ishaan.prazapati6375@gmail.com"><span className="span-11"><FaGooglePlusSquare /></span>Gmail</a></li>
                            <li><a href="https://linkedin.com/in/ishwor-prajapati-b4a102201"><span className="span-11"><FaLinkedin /> </span>Linkdin</a></li>
-                            <li><a href="https://linkedin.com/in/ishwor-prajapati-b4a102201"><span className="span-11"><FaFacebookSquare /></span> Facebook </a></li>
+                            <li><a href="https://www.facebook.com"><span className="span-11"><FaFacebookSquare /></span> Facebook </a></li>
                       </ul>
 
                   </div>
